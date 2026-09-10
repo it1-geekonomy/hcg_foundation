5ce73cdc46e2984076ed2ef061a98aaededae975
Sachin Nayak <sachinnayak9108@gmail.com>
2026-09-10 09:37:29 +0530

    Update database configuration and remove unused modules.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,20 +7,10 @@ import { ChatbotModule } from './chatbot/chatbot.module';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import configuration from './config/configuration';
 import { DatabaseModule } from './database/database.module';
-import { AnnualReportsModule } from './modules/annual-reports/annual-reports.module';
-import { ArticlesModule } from './modules/articles/articles.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { AwardsModule } from './modules/awards/awards.module';
-import { BlogsModule } from './modules/blogs/blogs.module';
-import { DonorsModule } from './modules/donors/donors.module';
 import { EventsModule } from './modules/events/events.module';
-import { GalleryModule } from './modules/gallery/gallery.module';
-import { LeadsContactModule } from './modules/leads-contact/leads-contact.module';
-import { LegalPagesModule } from './modules/legal-pages/legal-pages.module';
-import { NewslettersModule } from './modules/newsletters/newsletters.module';
 import { PatientStoriesModule } from './modules/patient-stories/patient-stories.module';
 import { ProjectsModule } from './modules/projects/projects.module';
-import { PublicationsModule } from './modules/publications/publications.module';
 import { TeamsModule } from './modules/teams/teams.module';
 import { TrusteesModule } from './modules/trustees/trustees.module';
 import { UsersModule } from './modules/users/users.module';
@@ -36,19 +26,9 @@ import { UsersModule } from './modules/users/users.module';
 
     TeamsModule,
     TrusteesModule,
-    AwardsModule,
-    AnnualReportsModule,
-    PublicationsModule,
-    NewslettersModule,
-    GalleryModule,
-    ArticlesModule,
-    BlogsModule,
     ProjectsModule,
     EventsModule,
     PatientStoriesModule,
-    DonorsModule,
-    LeadsContactModule,
-    LegalPagesModule,
     UsersModule,
     AuthModule,
 
